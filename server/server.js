52d4de56720f297beb16f19134240d0d7131e084
Nick <nick@krapf.net>
2023-02-04 10:27:58 -0700

    original

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX! (code-davinci-002)'
+    message: 'Hello from CodeX! (text-davinci-003)'
   })
 })
 
